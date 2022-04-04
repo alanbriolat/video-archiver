77600a9397756343d2ed7e77eaf11a354c109af3
Alan Briolat <alan@briol.at>
2022-04-04 20:32:54 +0100

    Make glade reflection recursive

--- gui/glade.go
+++ gui/glade.go
@@ -34,7 +34,7 @@ func MustBuildFromEmbed(s interface{}, filename string) {
 
 func MustBuild(s interface{}, builder *gtk.Builder) {
 	v := reflect.Indirect(reflect.ValueOf(s))
-	gladeFields := reflectGladeFields(v.Type())
+	gladeFields := reflectGladeFields(v.Type(), nil, "")
 	for _, gladeField := range gladeFields {
 		f := v.FieldByIndex(gladeField.index)
 		o := generic.Unwrap(builder.GetObject(gladeField.name))
@@ -49,22 +49,55 @@ type gladeField struct {
 
 func getGladeFields[T any]() []gladeField {
 	var v *T
-	return reflectGladeFields(reflect.TypeOf(v).Elem())
+	return reflectGladeFields(reflect.TypeOf(v).Elem(), nil, "")
 }
 
-// TODO: recursively get glade fields on nested/embedded structures?
-func reflectGladeFields(t reflect.Type) (out []gladeField) {
+var gobjectType = reflect.TypeOf((*glib.IObject)(nil)).Elem()
+
+func reflectGladeFields(t reflect.Type, indexPrefix []int, namePrefix string) (out []gladeField) {
 	out = make([]gladeField, 0, t.NumField())
 	for i := 0; i < t.NumField(); i++ {
 		f := t.Field(i)
-		if name, ok := f.Tag.Lookup("glade"); !ok {
+		log.Printf(
+			"%v.%v `%v`: exported=%v anonymous=%v kind=%v",
+			t.Name(),
+			f.Name,
+			f.Tag,
+			f.IsExported(),
+			f.Anonymous,
+			f.Type.Kind(),
+		)
+		name, hasTag := f.Tag.Lookup("glade")
+		name = namePrefix + name
+		if !hasTag {
+			// Ignore anything without a `glade:"..."` tag
 			continue
-		} else if name == "" {
-			log.Panicf("field '%v' of struct '%v' has empty glade tag (`glade:\"\"`)", f.Name, t)
 		} else if !f.IsExported() {
+			// Panic if a field is tagged but not exported - we won't be able to set its value, so best to fail
+			// loudly here instead of confusing the developer
 			log.Panicf("field '%v' of struct '%v' has `glade:\"%v\"` but is not exported", f.Name, t, name)
+		} else if f.Type.Kind() == reflect.Pointer && f.Type.Implements(gobjectType) {
+			// e.g. foo *gtk.Window `glade:"foo"`
+			if name == "" {
+				// Don't allow empty names, because presumably this would break gtk.Builder.GetObject()
+				log.Panicf("field '%v' of struct '%v' has empty glade tag (`glade:\"\"`)", f.Name, t)
+			}
+			out = append(out, gladeField{index: append(indexPrefix, i), name: name})
+		} else if f.Type.Kind() == reflect.Struct {
+			// e.g. InnerType `glade:"foo_"`, Inner someType `glade:"foo_"`
+			if reflect.PointerTo(f.Type).Implements(gobjectType) {
+				// Catch a possible common mistake: gtk.Widget instead of *gtk.Widget
+				log.Panicf(
+					"field '%v' of struct '%v' has unsupported type %v, expected *%v",
+					f.Name, t, f.Type, f.Type,
+				)
+			}
+			out = append(out, reflectGladeFields(f.Type, append(indexPrefix, i), name)...)
 		} else {
-			out = append(out, gladeField{index: []int{i}, name: name})
+			log.Panicf(
+				"field '%v' of struct '%v' has unsupported type for `glade:\"...\"` tag (%v)",
+				f.Name, t, f.Type,
+			)
 		}
 	}
 	return out
--- gui/glade_test.go
+++ gui/glade_test.go
@@ -3,6 +3,8 @@ package gui
 import (
 	"testing"
 
+	"github.com/gotk3/gotk3/gtk"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,8 +15,8 @@ func TestGetGladeFields(t *testing.T) {
 	assert.Equal([]gladeField{}, getGladeFields[empty]())
 
 	type hasFields struct {
-		Untagged interface{}
-		Correct  interface{} `glade:"correct"`
+		Untagged *gtk.Widget
+		Correct  *gtk.Widget `glade:"correct"`
 	}
 	assert.Equal(
 		[]gladeField{
@@ -24,12 +26,129 @@ func TestGetGladeFields(t *testing.T) {
 	)
 
 	type unexported struct {
-		foo interface{} `glade:"foo"`
+		foo *gtk.Widget `glade:"foo"`
 	}
 	assert.Panics(func() { getGladeFields[unexported]() }, "unexported but tagged field should panic")
 
 	type emptyTag struct {
-		Foo interface{} `glade:""`
+		Foo *gtk.Widget `glade:""`
 	}
 	assert.Panics(func() { getGladeFields[emptyTag]() }, "empty glade tag should panic")
+
+	type wrongType struct {
+		Foo int `glade:"foo"`
+	}
+	assert.Panics(func() { getGladeFields[wrongType]() }, "glade tag on wrong type should panic")
+
+	type wrongGTKType struct {
+		// A realistic example: forgetting to use a pointer to a GTK type
+		Foo gtk.Widget `glade:"foo"`
+	}
+	assert.Panics(func() { getGladeFields[wrongGTKType]() }, "glade tag on wrong type should panic")
+}
+
+func TestGetGladeFieldsEmbedded(t *testing.T) {
+	assert := assert.New(t)
+
+	// Inner type must be exported for an anonymous struct field using it to be exported
+	type Inner struct {
+		A *gtk.Widget `glade:"a"`
+		B *gtk.Widget `glade:"b"`
+	}
+
+	type ignored struct {
+		Inner
+		C *gtk.Widget `glade:"c"`
+		D *gtk.Widget `glade:"d"`
+	}
+	assert.Equal(
+		[]gladeField{
+			{[]int{1}, "c"},
+			{[]int{2}, "d"},
+		},
+		getGladeFields[ignored](),
+	)
+
+	type noPrefix struct {
+		Inner `glade:""`
+		C     *gtk.Widget `glade:"c"`
+		D     *gtk.Widget `glade:"d"`
+	}
+	assert.Equal(
+		[]gladeField{
+			{[]int{0, 0}, "a"},
+			{[]int{0, 1}, "b"},
+			{[]int{1}, "c"},
+			{[]int{2}, "d"},
+		},
+		getGladeFields[noPrefix](),
+	)
+
+	type withPrefix struct {
+		Inner `glade:"foo_"`
+		C     *gtk.Widget `glade:"c"`
+		D     *gtk.Widget `glade:"d"`
+	}
+	assert.Equal(
+		[]gladeField{
+			{[]int{0, 0}, "foo_a"},
+			{[]int{0, 1}, "foo_b"},
+			{[]int{1}, "c"},
+			{[]int{2}, "d"},
+		},
+		getGladeFields[withPrefix](),
+	)
+}
+
+func TestGetGladeFieldsNested(t *testing.T) {
+	assert := assert.New(t)
+
+	// No need to export inner type, because we export the struct field that uses it instead
+	type inner struct {
+		A *gtk.Widget `glade:"a"`
+		B *gtk.Widget `glade:"b"`
+	}
+
+	type ignored struct {
+		Inner inner
+		C     *gtk.Widget `glade:"c"`
+		D     *gtk.Widget `glade:"d"`
+	}
+	assert.Equal(
+		[]gladeField{
+			{[]int{1}, "c"},
+			{[]int{2}, "d"},
+		},
+		getGladeFields[ignored](),
+	)
+
+	type noPrefix struct {
+		Inner inner       `glade:""`
+		C     *gtk.Widget `glade:"c"`
+		D     *gtk.Widget `glade:"d"`
+	}
+	assert.Equal(
+		[]gladeField{
+			{[]int{0, 0}, "a"},
+			{[]int{0, 1}, "b"},
+			{[]int{1}, "c"},
+			{[]int{2}, "d"},
+		},
+		getGladeFields[noPrefix](),
+	)
+
+	type withPrefix struct {
+		Inner inner       `glade:"foo_"`
+		C     *gtk.Widget `glade:"c"`
+		D     *gtk.Widget `glade:"d"`
+	}
+	assert.Equal(
+		[]gladeField{
+			{[]int{0, 0}, "foo_a"},
+			{[]int{0, 1}, "foo_b"},
+			{[]int{1}, "c"},
+			{[]int{2}, "d"},
+		},
+		getGladeFields[withPrefix](),
+	)
 }
