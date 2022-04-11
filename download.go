36d4cac3da57e030ecea2ecda6305d6a45e2543d
Alan Briolat <alan@briol.at>
2022-04-11 10:03:47 +0100

    Make sure "raw" provider fails download on non-200 response

--- download.go
+++ download.go
@@ -107,6 +107,9 @@ func (d *download) SaveHTTPRequest(filename string, req *http.Request) error {
 	req = req.WithContext(d.Context())
 	client := http.Client{}
 	resp, err := client.Do(req)
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("404 not found")
+	}
 	if err != nil {
 		return fmt.Errorf("download failed: %w", err)
 	}
